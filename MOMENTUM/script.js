d470c336848ba916bd8dd160e6b3ecf2ebd13850
In Hwan Hwang <91361439+kok05206@users.noreply.github.com>
2023-03-06 19:11:17 +0900

    JavaScript_기초 변수
    - const : 값의 변동 X
    - let : 값의 변동 O
    - var은 되도록 사용 X
    - TYPE
    - null
    - undefined
    - boolean

---
+++ MOMENTUM/script.js
@@ -0,0 +1,27 @@
+console.log('HELLO WORLD!');
+console.log(120 * 5);
+console.log(120 + 5);
+console.log(120 - 5);
+
+// variable
+const a = 5;
+const b = '2';
+let myName = 'LEE';
+// constant : 값이 변하지 않음
+// let : 값이 변할 수 있음
+// var를 사용할 수 있는 반면, 그럼 value를 보호 받지 못함
+
+console.log(a * b);
+console.log(a + b);
+console.log(a - b);
+
+console.log('Hello! ' + myName);
+
+myName = 'inhwan';
+console.log('Hello! ' + myName);
+
+let check = false; // Boolean type
+const not = null; // null type
+let anything; // undefined type
+
+console.log(check, not, anything);
